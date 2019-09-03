79ad58f39794d8761fef0c2852803565d68fa6c0
Renan Fernandes Silva <renanfssilva@gmail.com>
2019-09-02 21:27:46 -0300

    Revert to come back to the original branch

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -1,28 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+<<<<<<< HEAD
 import { FormsModule } from '@angular/forms';
 <<<<<<< HEAD
 
 =======
 >>>>>>> parent of f7dad96... Empty project using Bootstrap
+=======
+>>>>>>> parent of befc965... First exercise - learning how to use components
 
 import { AppComponent } from './app.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SuccessAlertComponent,
-    WarningAlertComponent
+    AppComponent
   ],
   imports: [
+<<<<<<< HEAD
     BrowserModule,
 <<<<<<< HEAD
     FormsModule,
 =======
     FormsModule
 >>>>>>> parent of f7dad96... Empty project using Bootstrap
+=======
+    BrowserModule
+>>>>>>> parent of befc965... First exercise - learning how to use components
   ],
   providers: [],
   bootstrap: [AppComponent]
