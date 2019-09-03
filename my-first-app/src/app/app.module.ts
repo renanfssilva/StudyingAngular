e377faa9adb38719ab9ba2619ea5790472179d09
Renan Fernandes Silva <renanfssilva@gmail.com>
2019-09-02 21:23:46 -0300

    Revert to create an empty branch

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+<<<<<<< HEAD
 
+=======
+>>>>>>> parent of f7dad96... Empty project using Bootstrap
 
 import { AppComponent } from './app.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
@@ -15,7 +18,11 @@ import { SuccessAlertComponent } from './success-alert/success-alert.component';
   ],
   imports: [
     BrowserModule,
+<<<<<<< HEAD
     FormsModule,
+=======
+    FormsModule
+>>>>>>> parent of f7dad96... Empty project using Bootstrap
   ],
   providers: [],
   bootstrap: [AppComponent]
